da201d8b3f3983eb990d0ddbe18a3c6b04e0ac58
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2023-12-07 01:57:27 +0300

    API işlemleri.

---
+++ BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public String? Name { get; set; }
+    }
+}
